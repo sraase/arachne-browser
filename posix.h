88b5e76cb637da167131f56794eebfdbc669353d
Sebastian Raase <github@sraa.de>
2023-10-19 19:34:55 +0200

    posix: include some C99 fixed width integer types
    The current target environments (16-bit DOS, 32-bit Linux,
    64-bit Linux) use different C data types, causing major
    issues in places where the actual types matter. Especially
    the lack of a common 32-bit type is particularly painful.
    
    This commit introduced support for the fixed width integer
    data types introduced with C99. Unfortunately, Borland C++ 3.1
    (used for DOS builds) does not support them (no stdint.h),
    so they are manually typedef'd.
    
    Note that Borland C++ 3.1 does not support 64-bit types
    and will silently reduce "long long" to a 32-bit "long".

--- posix.h
+++ posix.h
@@ -28,15 +28,35 @@
 #include <string.h>
 #include <math.h>
 
+#ifdef __BORLANDC__
+/* define some C99 fixed width integer types,
+   because Borland C++ 3.1 lacks stdint.h */
+typedef signed   char  int8_t;
+typedef unsigned char  uint8_t;
+typedef signed   short int16_t;
+typedef unsigned short uint16_t;
+typedef signed   long  int32_t;
+typedef unsigned long  uint32_t;
+#if defined(__COMPACT__) || defined(__LARGE__) || defined(__HUGE__)
+typedef signed   long  intptr_t;
+typedef unsigned long  uintptr_t;
+#else
+typedef signed   short intptr_t;
+typedef unsigned short uintptr_t;
+#endif
+#else
+#include <stdint.h>
+#endif
+
 #define PATHSEP '\\'
 
 #elif LINUX
 //==========================================================================
 
-//....  will include standard header files later ....
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/time.h>
